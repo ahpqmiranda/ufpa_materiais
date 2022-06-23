d0c21ff4262757bcf94f0dff853b2fcbb9a1b650
Alan <alan.adm2011@gmail.com>
2022-06-23 01:47:40 -0300

    23/06: novas correções

---
+++ Mecânica dos Fluidos/teste_colebroke.py
@@ -0,0 +1,109 @@
+import numpy as np
+import pandas as pd
+import sympy as sp
+from random import randint
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+sp.init_printing(use_latex='png', scale=1.0, order='grlex',
+                 forecolor='Black', backcolor='White')
+x = sp.Symbol('x')
+
+E = 1.5e-4  # [m] -> Rugosidade da parede interna do duto
+
+Din = 1.22  # [m] -> Diâmetro interno do duto
+
+re = 1.7e+5  # n.º de reynolds para o fluido em questão
+
+rho = 998  # [kg/m3] -> Densidade do fluido (agua)
+
+mu = 0.001  # [Pa/s] -> Viscosidade dinamica do fluido (agua)
+
+v = 1.0  # [mm/s] -> Velocidade do escoamento
+
+Re = rho * Din * v / mu  # [] -> Numero de Reynolds
+# (função) fator de atrito
+start = 1
+end = 100
+spaces = 100
+delta = 1
+f_lst = []
+g_lst = []
+gprime_lst = []
+f0 = 0.009
+guess = f0
+f0 = randint(1, 10) / 1000  # Palpite inicial (numero aleatorio)
+tolerancia = 1.00e-08
+
+
+def g(f):
+    return 1 / (f ** 0.5) + 2.0 * np.log10(E / (3.7 * Din) + 2.51 / (Re * f ** 0.5))
+
+
+def gprime(f):
+    return -0.5 * f ** (-1.5) - 2.51 * f ** (-1.5) / (
+            Re * (2.51 * f ** (-0.5) / Re + 0.27027027027027 * E / Din) * np.log(10))
+
+
+while delta > tolerancia:
+    f = guess - ((g(guess)) / (gprime(guess)))
+    delta = abs(f - guess)
+    f_lst.append(f)
+    g_lst.append(g(f))
+    gprime_lst.append(gprime(f))
+    guess = f
+
+data_df = pd.DataFrame(list(zip(f_lst, g_lst, gprime_lst)),
+                       columns=['fi', 'g(f)', 'g\'(f)'])
+
+plt.style.use('seaborn-bright')
+fig = plt.figure(figsize=(10, 6))
+
+m1 = np.array(data_df['fi'], dtype=float)
+m2 = np.array(data_df['g(f)'], dtype=float)
+m3 = np.array(data_df['g\'(f)'], dtype=float)
+x = np.array(range(len(m1)))
+
+plt.plot(x, m1, 'r')
+plt.plot(x, m2, 'g')
+plt.plot(x, m3, 'b')
+plt.legend(["fator de atrito", "eq. colebroke", "erro associado"])
+plt.axis([0, len(m1), 0, max([max(m1), max(m2), max(m3)])])
+plt.xlabel('Iterações')
+plt.ylabel('Aproximações')
+plt.title('Método de Newton Raphson')
+plt.grid(True)
+plt.show()
+
+rugosidade = lambda f: 1 / (f ** 0.5) + 2.0 * np.log10(E / (3.7 * Din) + 2.51 / (Re * f ** 0.5))
+list_rugosidade = []
+
+for x in np.linspace(start, end, spaces):
+    list_rugosidade.append(rugosidade(x))
+
+x_interval = np.log(np.linspace(start, end, spaces))
+xdic = {'X': np.exp(x_interval)}
+ydic = {'Y': np.exp(list_rugosidade)}
+
+X = pd.DataFrame.from_dict(xdic)
+Y = pd.DataFrame.from_dict(ydic)
+
+x_SEQ = np.linspace(X.min(), X.max(), spaces).reshape(-1, 1)
+
+value = 4  # grau do polinômio
+
+polynomial_regression = make_pipeline(PolynomialFeatures(value), LinearRegression())
+polynomial_regression.fit(X, Y)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(X, Y)
+plt.plot(x_SEQ, polynomial_regression.predict(x_SEQ), color='hotpink')
+plt.title('Regressão Polinomial (log x log) de ordem: ' + str(value))
+plt.grid(True)
+plt.legend(["Dados amostrais", "Interpolador Polinomial"])
+plt.xlabel("Rugosidade")
+plt.ylabel("Perda de Carga")
+plt.show()
